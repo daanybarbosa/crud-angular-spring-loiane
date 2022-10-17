7d41ffcacc092ac48d3f1104e535c33b992cfc3e
Daniele Barbosa <daanybsilva@gmail.com>
2022-10-17 18:40:40 -0300

    aula 26 - API String: Buscar por ID

--- crud-spring/src/main/java/com/loiane/controller/CourseController.java
+++ crud-spring/src/main/java/com/loiane/controller/CourseController.java
@@ -3,7 +3,9 @@ package com.loiane.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,40 +30,32 @@ import lombok.AllArgsConstructor;
  *  - A vantagem de utilizar o ResponseEntity é caso precise manipular o header/cabeçalho/informações do Response
  * @ResponseStatus - é uma alternativa ao @ResponseEntity, é um código padrão que será retornado pelo o método
  *  - Parametros: código (status do HTTP) e o motivo (é mais utilizado quando vai tratar)
+ * @PathVariable - variavel que está no caminho da URL, ou seja, uma parte da URL
  */
 @RestController
 @RequestMapping("/api/courses")
 @AllArgsConstructor
 public class CourseController {
-    
+
     private final CourseRepository courseRepository;
 
     @GetMapping
-    public @ResponseBody List<Course> list(){
+    public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
     }
 
-    /*@PostMapping 
-    public void create(@RequestBody Course course){
-       //System.out.println(course.getName());
-       courseRepository.save(course); //salvar o objeto no Banco de dados
-    } */
-
-    /*@PostMapping
-    public Course create(@RequestBody Course course){
-        //System.out.println(course.getName());
-        return courseRepository.save(course);
-     } */
-
-    /*@PostMapping
-    public ResponseEntity<Course> create(@RequestBody Course course){
-        return ResponseEntity.status(HttpStatus.CREATED)
-            .body(courseRepository.save(course)); //status 201 - Created
-     } */
+    // Buscar por ID
+    // URI - é uma parte da url
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody Course course){
+    public Course create(@RequestBody Course course) {
         return courseRepository.save(course);
-     }
+    }
 }
